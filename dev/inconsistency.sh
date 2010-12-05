1614ac9aae8b8f572fb3b2e0224555a90a62b63d
Francis M. Tyers <ftyers@prompsit.com>
2010-12-05 12:59:45 +0000

    summaries

--- dev/inconsistency.sh
+++ dev/inconsistency.sh
@@ -2,7 +2,7 @@ TMPDIR=/tmp
 
 if [[ $1 = "ast-es" ]]; then
 
-lt-expand ../apertium-es-ast.es.dix | grep -v REGEX | grep -v '<prn><enc>' | sed 's/:>:/%/g' | grep -v ':<:' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
+lt-expand ../apertium-es-ast.ast.dix | grep -v REGEX | grep -v '<prn><enc>' | sed 's/:>:/%/g' | grep -v ':<:' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
         apertium-pretransfer|
         apertium-transfer ../apertium-es-ast.ast-es.t1x  ../ast-es.t1x.bin  ../ast-es.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt |
         lt-proc -d ../ast-es.autogen.bin > $TMPDIR/tmp_testvoc3.txt
@@ -10,7 +10,7 @@ paste -d _ $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc
 
 elif [[ $1 = "es-ast" ]]; then
 
-lt-expand ../apertium-es-ast.ast.dix | grep -v REGEX | grep -v '<prn><enc>' | sed 's/:>:/%/g' | grep -v ':<:' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
+lt-expand ../apertium-es-ast.es.dix | grep -v REGEX | grep -v '<prn><enc>' | sed 's/:>:/%/g' | grep -v ':<:' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
         apertium-pretransfer|
         apertium-transfer ../apertium-es-ast.es-ast.t1x  ../es-ast.t1x.bin  ../es-ast.autobil.bin | tee $TMPDIR/tmp_testvoc2.txt |
         lt-proc -d ../es-ast.autogen.bin > $TMPDIR/tmp_testvoc3.txt
